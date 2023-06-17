56a782b8f0215b55abf52a992e18dc7ff00de418
AmadeuszKosedek <42908187+AmadeuszKosedek@users.noreply.github.com>
2023-06-17 11:55:25 +0200

    Add files via upload

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,8 @@
-"""mysite URL Configuration
+"""
+URL configuration for carbon_ai project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from carbon_ai_app import home
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
+    path('', home, name='home'),  # Dodaj tę linię dla strony głównej
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
